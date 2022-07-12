0ce531101808e135d82529109d700bbc600aa85d
Rohit-Saini7 <rohit712wd@gmail.com>
2022-07-12 17:50:25 +0530

    styling done(with responsiveness)

--- src/App.js
+++ src/App.js
@@ -4,71 +4,95 @@ function App() {
   return (
     <div>
       <div className="top-section">
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing.</h1>
-        <h2>
+        <h1 className="main-heading">
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        </h1>
+        <h2 className="main-subheading">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          eiusmod tempor incididunt.
         </h2>
-        <div className="form-heading">WHAT WE OFFER</div>
-        <div className="waht-we-offer">
-          <input type="radio" name="item" id="item1" />
-          <label>Lorem ipsum dolor</label>
-          <input type="radio" name="item" id="item2" />
-          <label>Lorem ipsum dolor</label>
-          <input type="radio" name="item" id="item3" />
-          <label>Lorem ipsum dolor</label>
-          <input type="radio" name="item" id="item4" />
-          <label>Lorem ipsum dolor</label>
-          <input type="radio" name="item" id="item5" />
-          <label>Lorem ipsum dolor</label>
-          <input type="radio" name="item" id="item6" />
-          <label>Lorem ipsum dolor</label>
+        <div className="offer-box">
+          <div className="offer-box-heading">WHAT WE OFFER</div>
+          <div className="item">
+            <input type="radio" name="item" id="item1" />
+            <label>Lorem ipsum dolor</label>
+          </div>
+          <div className="item">
+            <input type="radio" name="item" id="item2" />
+            <label>Lorem ipsum dolor</label>
+          </div>
+          <div className="item">
+            <input type="radio" name="item" id="item3" />
+            <label>Lorem ipsum dolor</label>
+          </div>
+          <div className="item">
+            <input type="radio" name="item" id="item4" />
+            <label>Lorem ipsum dolor</label>
+          </div>
+          <div className="item">
+            <input type="radio" name="item" id="item5" />
+            <label>Lorem ipsum dolor</label>
+          </div>
+          <div className="item">
+            <input type="radio" name="item" id="item6" />
+            <label>Lorem ipsum dolor</label>
+          </div>
         </div>
         <div className="personal-details">
-          <h4>Personal Details</h4>
-          <label>Name</label>
-          <input type="text" name="Name" placeholder="enter your name" />
-          <label>Mobile Number</label>
-          <input
-            type="text"
-            name="mobile-number"
-            placeholder="enter your mobile Number"
-          />
-          <select value={"A"} onChange={"A"}>
-            <option value="Select City">Select City</option>
-            <option value="Lorem">Lorem</option>
-            <option value="ipsum">ipsum</option>
-            <option value="dolor">dolor</option>
-            <option value="tempor">tempor</option>
-          </select>
+          <h4 className="personal-details-heading">Personal Details</h4>
+          <div>
+            <input type="text" required="required" />
+            <span>Name</span>
+          </div>
+          <div>
+            <input type="text" required="required" />
+            <span>Mobile Number</span>
+          </div>
+          <div>
+            <select>
+              <option value="Select City">Select city</option>
+              <option value="Lorem">lorem</option>
+              <option value="ipsum">ipsum</option>
+              <option value="dolor">dolor</option>
+              <option value="tempor">tempor</option>
+            </select>
+          </div>
           <button>CONTINUE</button>
         </div>
       </div>
       <div className="middle-section">
-        <div className="product-description">
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <h4>Lorem ipsum</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <p>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
-          <img src="./images/scooter-image.png" alt="" />
-        </div>
+        <h2 className="main-subheading">
+          Lorem ipsum dolor sit amet adipiscing elit.
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam quis nostrud exercitation ullamco laboris nisi ut
+          aliquip.
+        </p>
+        <h3>Lorem ipsum</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src="./images/scooter-image.png" alt="" />
       </div>
       <div className="bottom-section">
-        <h3>Lorem ipsum dolor sit amet</h3>
+        <h2 className="main-subheading">Lorem ipsum dolor sit amet</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt
